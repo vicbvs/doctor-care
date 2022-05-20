04cece6c32a17b20760198cb2aa1eef960e8121a
Victor Borsatto <victor.borsattobh@gmail.com>
2022-05-19 23:37:28 -0300

    Finished style of mobile layout

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+window.addEventListener('scroll', onScroll);
+onScroll();
+
 function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) navigation.classList.add('scroll');
   else navigation.classList.remove('scroll');
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 400) backToTopButton.classList.add('show');
+  else backToTopButton.classList.remove('show');
 }
 
 function openMenu() {
@@ -10,3 +23,19 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded');
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card,
+  #about,
+  #about header,
+  #about content
+`);
